d38d8cc94bd7c38bdd6b22dd33a49ff61e5d2b43
bastienwcs <email>
2019-06-11 09:06:14 +0200

    init

---
+++ src/main/java/fr/wildcodeschool/robinsdesmers/RobinsdesmersApplication.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.robinsdesmers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobinsdesmersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobinsdesmersApplication.class, args);
+	}
+
+}
