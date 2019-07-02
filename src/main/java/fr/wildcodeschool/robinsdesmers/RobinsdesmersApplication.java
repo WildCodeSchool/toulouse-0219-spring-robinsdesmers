f92a67afb449d9850ac8b33388699ff477925339
bastienwcs <email>
2019-07-02 10:54:01 +0200

    conf for release

--- src/main/java/fr/wildcodeschool/robinsdesmers/RobinsdesmersApplication.java
+++ src/main/java/fr/wildcodeschool/robinsdesmers/RobinsdesmersApplication.java
@@ -7,11 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class RobinsdesmersApplication {
+public class RobinsdesmersApplication extends SpringBootServletInitializer {
 
 	@Autowired
 	UserRepository userRepository;
